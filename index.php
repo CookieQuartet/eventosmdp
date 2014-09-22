2ae32580dfd088adb3ec0c895cc0d0fc439625d9
Martin Maestri <mmaestri@gmail.com>
2014-09-22 01:41:19 -0300

    Inclusión de angular y material design

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
-<?php phpinfo()  ?>
+<!DOCTYPE html>
+<html ng-app>
+    <head>
+        <meta name="viewport" content="initial-scale=1, maximum-scale=1, user-scalable=no" />
+        <link rel="stylesheet" href="bower_components/angular-material/angular-material.css">
+    </head>
+    <body ng-app="EventosMDP">
+
+        <script src="bower_components/angular/angular.js"></script>
+        <script src="bower_components/angular-animate/angular-animate.js"></script>
+        <script src="bower_components/hammerjs/hammer.js"></script>
+        <script src="bower_components/angular-material/angular-material.js"></script>
+
+    </body>
+</html>
