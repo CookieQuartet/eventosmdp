49f1f6ab32970190206d79d15e484cb7bed7c8e1
MauLopez <lopezmf93@gmail.com>
2014-10-04 20:45:07 -0300

    Querys de comment

--- backend/alert/Alert.php
+++ backend/alert/Alert.php
@@ -9,4 +9,4 @@
 class Alert {
     private $id;
 
-} 
+}
--- backend/comment/CommentQuerys.php
+++ backend/comment/CommentQuerys.php
@@ -7,5 +7,40 @@
  */
 
 class CommentQuerys {
+    public final static function getCommentList($connection) //Lista de Usuarios
+    {
+
+        try {
+            $comment = $connection->query ("select * from COMMENT");
+        } catch (Exception $e) {
+            echo ($e);
+        }
+
+        return $comment;
+    }
+
+    public final static function addComment($connection, Comment $comment)
+    {
+        try {
+
+            $commentQuery = "insert into COMMENT (id, text, id_comment_status, id_event, stars) values ('$comment->getId()' , '$comment->getText()' , '$comment->getIdCommentStatus()', '$comment->getIdEvent()', '$comment->getStars()' )";
+            mysqli_query($connection, $commentQuery);
+
+        } catch (Exception $e) {
+            echo ($e);
+        }
+    }
+
+    public final static function updateComment($connection, Comment $comment)
+    {
+        try {
+
+            $commentQuery = "update COMMENT set 'id'=$comment->getId(), 'text'=$comment->getText(), 'id_comment_status'=$comment->getIdCommentStatus, 'id_event'=$comment->getIdEvent(), 'stars'=$comment->getStars() where 1";
+            mysqli_query($connection, $commentQuery);
+
+        } catch (Exception $e) {
+            echo ($e);
+        }
+    }
 
 } 
