e6089e303288b0003cc452f3450c041a5d716843
Julián <julianfalcionelli@gmail.com>
2014-10-04 20:29:41 -0300

    Querys Base de User

--- backend/user/UserQuerys.php
+++ backend/user/UserQuerys.php
@@ -4,4 +4,35 @@
  * User: Mauricio
  * Date: 04/10/2014
  * Time: 05:27 PM
- */ 
+ */
+class UserQuerys {
+
+    public final static function getUserList($connection) //Lista de Usuarios
+    {
+
+        try {
+            $users = $connection->query ("select * from USER");
+        } catch (Exception $e) {
+            echo ($e);
+        }
+
+        return $users;
+    }
+
+    public final static function addUser($connection, User $user)
+    {
+
+//        $password=$userValues['user_password'];
+
+        try {
+
+            $userQuery = "insert into USER (name, email, password, id_user_type, fcbk_token) values ('$user->getName()' , '$user->getEmail()' , '$user->getPassword()', '$user->getUserType()', '$user->getFcbkToken()' )";
+            mysqli_query($connection, $userQuery);
+
+        } catch (Exception $e) {
+            echo ($e);
+        }
+
+    }
+
+}
