1212948a2b6b4f618239dd1b2a6bc77ad743e4fd
Julián <julianfalcionelli@gmail.com>
2014-10-04 23:22:59 -0300

    user queryss

--- backend/user/UserQuerys.php
+++ backend/user/UserQuerys.php
@@ -19,55 +19,27 @@ class UserQuerys {
 
     public final static function addUser(User $user)
     {
-
-//        $password=$userValues['user_password'];
-
-        try {
-
-            $userQuery = "insert into USER (name, email, password, id_user_type, fcbk_token) values ('$user->getName()' , '$user->getEmail()' , '$user->getPassword()', '$user->getUserType()', '$user->getFcbkToken()' )";
-            mysqli_query($connection, $userQuery);
-
-        } catch (Exception $e) {
-            echo ($e);
-        }
+        $dataBase = new DataBase();
+        return $dataBase->query("insert into USER (name, email, password, id_user_type, fcbk_token) values ('$user->getName()' , '$user->getEmail()' , '$user->getPassword()', '$user->getUserType()', '$user->getFcbkToken()' )");
     }
 
     public final static function updateUser($connection, User $user)
     {
-        try {
-
-            $commentQuery = "update USER set 'name'=$user->getName(), 'email'=$user->getEmail(), 'password'=$user->getPassword(), 'id_user_type'=$user->getUserType(), 'fcbk_token'=$user->getFcbkToken(), 'active'=$user->getActive() where 'id'=$user->getId()";
-            mysqli_query($connection, $commentQuery);
-
-        } catch (Exception $e) {
-            echo ($e);
-        }
+        $dataBase = new DataBase();
+        return $dataBase->query("update USER set 'name'=$user->getName(), 'email'=$user->getEmail(), 'password'=$user->getPassword(), 'id_user_type'=$user->getUserType(), 'fcbk_token'=$user->getFcbkToken(), 'active'=$user->getActive() where 'id'=$user->getId()");
     }
 
-    public final static function deleteUser($connection, $id)
+    public final static function deleteUser($id)
     {
-
-        try {
-
-            $userQuery = "update USER set active = '0' where id = '$id'";
-            mysqli_query($connection, $userQuery);
-
-        } catch (Exception $e) {
-            echo ($e);
-        }
-
+        $dataBase = new DataBase();
+        return $dataBase->query("update USER set active = '0' where id = '$id'");
     }
 
     public final static function getUserById($connection, $id)
     {
 
-        try {
             $user = $connection->query("select * from `USER` WHERE id = $id");
-        } catch (Exception $e) {
-            echo($e);
-        }
 
-        return $user;
     }
 
 }
