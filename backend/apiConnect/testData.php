e996d9fe5ae152f68b2092cb6855974ecbb237ac
Martin Maestri <mmaestri@gmail.com>
2014-10-11 18:01:26 -0300

    agregué testData.php

---
+++ backend/apiConnect/testData.php
@@ -0,0 +1,28 @@
+<?php
+    $url = "http://appsb.mardelplata.gob.ar/consultas/wsCalendario/RESTServiceCalendario.svc/calendario/consultaEventos";
+    $data = array(
+        "Token" => "3C37175138FF30B0B7849684A4AC8335",
+        "IdArea" => 2,
+        "IdSubarea" => 2,
+        "Palabra" => null,
+        "FechaDesde" => null,
+        "FechaHasta" => null
+    );
+    $data_string = json_encode($data);
+
+
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+        'Content-Type: application/json',
+        'Content-Length: ' . strlen($data_string))
+    );
+
+    $result = curl_exec($ch);
+
+    //$aresult = json_decode($result);
+
+    echo($result);
+
