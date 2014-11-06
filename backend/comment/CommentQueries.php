92c3699b53bd6c2bcd4ff92a627157b9e101d531
PADomine <padomine@gmail.com>
2014-11-05 23:36:54 -0300

    New queries for Comments

--- backend/comment/CommentQueries.php
+++ backend/comment/CommentQueries.php
@@ -17,26 +17,57 @@ class CommentQueries {
         $this->dataBase = new DataBase();
     }
 
-    public final function getCommentList() //Lista de Comentarios
+    public final function getCommentList() //Lista de Comentarios Completa
     {
-        return $this->dataBase->query("select CO.*,SC.description from COMMENT CO, STATUS_COMMENT SC where CO.id_status_comment = SC.id");
+        $commentQuery = "select CO.*,SC.description from COMMENT CO, STATUS_COMMENT SC where CO.idStatusComment = SC.id";
+        return $this->dataBase->query($commentQuery);
+    }
+
+    public final function getCommentListPending() //Lista de Comentarios Pendientes
+    {
+        $commentQuery = "select CO.*,SC.description from COMMENT CO, STATUS_COMMENT SC where CO.idStatusComment = SC.id and SC.id = 1";
+        return $this->dataBase->query($commentQuery);
+    }
+
+    public final function getCommentListReported() //Lista de Comentarios Denunciados
+    {
+        $commentQuery = "select CO.*,SC.description from COMMENT CO, STATUS_COMMENT SC where CO.idStatusComment = SC.id and SC.id = 3";
+        return $this->dataBase->query($commentQuery);
+    }
+
+    public final function getCommentListForEvent($idEvent) //Lista de Comentarios Aprobados para un Evento
+    {
+        $commentQuery = "select CO.*,SC.description from COMMENT CO, STATUS_COMMENT SC where CO.idStatusComment = SC.id  and SC.id = 2 and CO.id_event = '$idEvent'";
+        return $this->dataBase->query($commentQuery);
     }
 
     public final function addComment($comment)
     {
-        $commentQuery = "insert into COMMENT (text, id_status_comment, id_event, stars) values ('$comment->getText()' , '$comment->getIdCommentStatus()', '$comment->getIdEvent()', '$comment->getStars()' )";
+        $commentQuery = "insert into COMMENT (text, idStatusComment, id_event, stars) values ('$comment->getText()' , '$comment->getIdCommentStatus()', '$comment->getIdEvent()', '$comment->getStars()' )";
         return $this->dataBase->query($commentQuery);
     }
 
     public final function updateComment($comment)
     {
-        $commentQuery = "update COMMENT set text='$comment->getText()', id_status_comment='$comment->getIdCommentStatus()', id_event='$comment->getIdEvent()', stars='$comment->getStars()' where id='$comment->getId()'";
+        $commentQuery = "update COMMENT set text='$comment->getText()', idStatusComment='$comment->getIdCommentStatus()', id_event='$comment->getIdEvent()', stars='$comment->getStars()' where id='$comment->getId()'";
         return $this->dataBase->query($commentQuery);
     }
 
-    public final function deleteComment($id)
+    public final function approveComment($id) //Set Comentario Aprobado
     {
-        $commentQuery = "update COMMENT set active = '0' where id = '$id'";
+        $commentQuery = "update COMMENT set idStatusComment = '2' where id = '$id'";
+        return $this->dataBase->query($commentQuery);
+    }
+
+    public final function reportComment($id) //Set Comentario Denunciado
+    {
+        $commentQuery = "update COMMENT set idStatusComment = '3' where id = '$id'";
+        return $this->dataBase->query($commentQuery);
+    }
+
+    public final function deleteComment($id) //Set Comentario Eliminado
+    {
+        $commentQuery = "update COMMENT set idStatusComment = '4' where id = '$id'";
         return $this->dataBase->query($commentQuery);
     }
 
