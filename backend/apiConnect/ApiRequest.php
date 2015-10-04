0cbe8e63ced6eb728c79c59e8a71739ebf8c4154
Julián <julianfalcionelli@gmail.com>
2015-10-04 18:43:12 -0300

    Queries fixes. API Get Events Fix.

--- backend/apiConnect/ApiRequest.php
+++ backend/apiConnect/ApiRequest.php
@@ -48,6 +48,10 @@ class ApiRequest {
 
     public function getEventsBySubarea($idSubArea, $fechaDesde)
     {
+        if ($fechaDesde == null)
+        {
+            $fechaDesde = "20150909T000000";
+        }
         $url = consultaEventosUrl;
         $data = array( "Token" => defaultToken, "IdArea" => idAreaCultura, "IdSubarea" => $idSubArea, "Palabra" => null, "FechaDesde" => $fechaDesde, "FechaHasta" => null);
         $data_string = json_encode($data);
@@ -66,12 +70,11 @@ class ApiRequest {
         //$aresult = json_decode(utf8_encode($result));
         //$aresult = json_decode(iconv( "ISO-8859-1", "UTF-8", $result ));
 
-
-        var_dump($aresult);
+      //  var_dump($aresult);
 
         if ($aresult->Estado==warning)
         {
-           // return $aresult->Eventos;
+            //return $aresult->Eventos;
             return array_map("unserialize", array_unique(array_map("serialize",array_merge ($aresult->Eventos,$this->getEventsBySubarea($idSubArea, end($aresult->Eventos)->FechaHoraInicio)))));
         }
         elseif ($aresult->Estado==sucessfull)
